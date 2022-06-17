daebb4a5f7a1c2477c6b75520c1dc22349523b2e
Cody McCarty <codyjmccarty@gmail.com>
2022-06-16 21:06:43 -0400

    initial commit

---
+++ src/main/java/io/example/democoffeepotservicerest/DemoCoffeePotServiceRestApplication.java
@@ -0,0 +1,13 @@
+package io.example.democoffeepotservicerest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCoffeePotServiceRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCoffeePotServiceRestApplication.class, args);
+	}
+
+}
