8e0c816a0f60680ba327bf86a6c32256bf6a8d82
Cody McCarty <codyjmccarty@gmail.com>
2022-06-18 12:24:00 -0400

    basic swagger

--- src/main/java/io/example/democoffeepotservicerest/DemoCoffeePotServiceRestApplication.java
+++ src/main/java/io/example/democoffeepotservicerest/DemoCoffeePotServiceRestApplication.java
@@ -2,8 +2,10 @@ package io.example.democoffeepotservicerest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class DemoCoffeePotServiceRestApplication {
 
 	public static void main(String[] args) {
